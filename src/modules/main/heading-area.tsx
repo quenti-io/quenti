b648039df5c5d930d9ef5f6e217a2b6a476f14d2
Ethan Lerner <ethan@lerners.io>
2023-01-30 18:15:57 -0500

    start of set visibility

--- src/modules/main/heading-area.tsx
+++ src/modules/main/heading-area.tsx
@@ -1,4 +1,11 @@
-import { Heading, Stack, Text, useColorModeValue } from "@chakra-ui/react";
+import {
+  Heading,
+  HStack,
+  Stack,
+  Text,
+  useColorModeValue,
+} from "@chakra-ui/react";
+import { IconWorld } from "@tabler/icons-react";
 import { useSet } from "../../hooks/use-set";
 
 export const HeadingArea = () => {
@@ -8,9 +15,16 @@ export const HeadingArea = () => {
   return (
     <Stack spacing={4} marginTop="10">
       <Heading size="2xl">{title}</Heading>
-      <Text color={text} fontWeight={600}>
-        {terms.length} term{terms.length != 1 ? "s" : ""}
-      </Text>
+      <HStack color={text} fontWeight={600} spacing={2}>
+        <HStack>
+          <IconWorld size={18} />
+          <Text>Public</Text>
+        </HStack>
+        <Text>•</Text>
+        <Text>
+          {terms.length} term{terms.length != 1 ? "s" : ""}
+        </Text>
+      </HStack>
     </Stack>
   );
 };
