8167b9b04da12a9ff0cf24e79290187e8a91204d
Ethan Lerner <ethan@lerners.io>
2023-02-02 19:40:50 -0500

    user profile link

--- src/modules/main/description-area.tsx
+++ src/modules/main/description-area.tsx
@@ -1,9 +1,19 @@
-import { Avatar, Box, HStack, Stack, Text, Tooltip } from "@chakra-ui/react";
+import {
+  Avatar,
+  Box,
+  HStack,
+  Link,
+  Stack,
+  Text,
+  Tooltip,
+  useColorModeValue,
+} from "@chakra-ui/react";
 import { IconDiscountCheck } from "@tabler/icons-react";
 import { useSet } from "../../hooks/use-set";
 
 export const DescriptionArea = () => {
   const { description, user } = useSet();
+  const highlight = useColorModeValue("blue.500", "blue.200");
 
   return (
     <Stack spacing={8}>
@@ -12,7 +22,13 @@ export const DescriptionArea = () => {
         <Stack spacing={0}>
           <Text fontSize="xs">Created by</Text>
           <HStack spacing="2">
-            <Text fontWeight={700}>{user.username}</Text>
+            <Link
+              fontWeight={700}
+              href={`/@${user.username}`}
+              _hover={{ color: highlight }}
+            >
+              {user.username}
+            </Link>
             {user.verified && (
               <Box color="blue.300">
                 <Tooltip label="Verified">
