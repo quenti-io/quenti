7976fe3ffa028f9d55f29c901a2f2fdd49f3e1f8
Ethan Lerner <ethan@lerners.io>
2023-02-12 20:35:36 -0500

    fix keyboard bug

--- src/modules/learn/any-key-press-layer.tsx
+++ src/modules/learn/any-key-press-layer.tsx
@@ -3,6 +3,7 @@ import { useShortcut } from "../../hooks/use-shortcut";
 export const AnyKeyPressLayer = ({ onSubmit }: { onSubmit: () => void }) => {
   useShortcut([], onSubmit, {
     ctrlKey: false,
+    anyKey: true,
     allowInput: true,
   });
   return null;
