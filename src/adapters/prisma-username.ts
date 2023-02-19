ba80da212446a41d71633c8aad06fb1f9aa6d08d
Ethan Lerner <ethan@lerners.io>
2023-02-19 13:31:55 -0500

    case insensitive user query

--- src/adapters/prisma-username.ts
+++ src/adapters/prisma-username.ts
@@ -14,6 +14,7 @@ export function CustomPrismaAdapter(p: PrismaClient): Adapter {
       const existing = await p.user.findMany({
         where: {
           username: {
+            mode: "insensitive",
             startsWith: sanitized,
           },
         },
