0236138e161b9b4a78d12167b200054aaf2124ce
Ethan Lerner <ethan@lerners.io>
2023-02-23 14:44:34 -0500

    add error page

---
+++ src/pages/_error.tsx
@@ -0,0 +1,39 @@
+import { Button, Center, Heading, Text, VStack } from "@chakra-ui/react";
+import Head from "next/head";
+
+export default function Error() {
+  return (
+    <>
+      <Head>
+        <title>Error | Quizlet.cc</title>
+      </Head>
+      <Center height="calc(100vh - 120px)">
+        <VStack textAlign="center" px="4">
+          <Heading
+            fontSize={{ base: "6xl", md: "8xl", lg: "9xl" }}
+            bgGradient="linear(to-r, blue.300, purple.300)"
+            bgClip="text"
+          >
+            Oh Snap!
+          </Heading>
+          <VStack spacing={6}>
+            <Text
+              fontSize={{ base: "md", sm: "lg" }}
+              color="gray.500"
+              fontWeight={600}
+            >
+              An error occured on this page that we couldn&apos;t handle.
+            </Text>
+            <Button
+              onClick={() => {
+                window.location.reload();
+              }}
+            >
+              Reload
+            </Button>
+          </VStack>
+        </VStack>
+      </Center>
+    </>
+  );
+}
