368b42d5375562bf67057ebed752bd57f803ac36
Ethan Lerner <ethan@lerners.io>
2023-02-26 12:54:46 -0500

    fix landing page spacing

--- src/components/static-wrapper.tsx
+++ src/components/static-wrapper.tsx
@@ -4,7 +4,7 @@ export const StaticWrapper: React.FC<React.PropsWithChildren> = ({
   children,
 }) => {
   return (
-    <Box w="full" minH="calc(100vh - 80px)" bg="gray.900">
+    <Box w="full" minH="calc(100vh - 80px)" bg="gray.900" overflowX="hidden">
       {children}
     </Box>
   );
