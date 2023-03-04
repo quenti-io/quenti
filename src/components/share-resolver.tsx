61de7138724369cb3259026c85cb185557d605b2
Ethan Lerner <ethan@lerners.io>
2023-03-04 15:04:11 -0500

    share 404

--- src/components/share-resolver.tsx
+++ src/components/share-resolver.tsx
@@ -1,15 +1,17 @@
+import type { EntityType } from "@prisma/client";
 import { useRouter } from "next/router";
 import React from "react";
-import { Loading } from "./loading";
 import { Folder404 } from "../modules/folders/folder-404";
 import { api } from "../utils/api";
+import { Generic404 } from "./generic-404";
+import { Loading } from "./loading";
 
 export const ShareResolver = () => {
   const router = useRouter();
   const id = router.query.id as string;
   const sanitized = (id || "").slice(1);
 
-  const [resolved, setResolved] = React.useState(false);
+  const [resolved, setResolved] = React.useState<EntityType | "404">();
 
   api.shareResolver.query.useQuery(sanitized, {
     retry: false,
@@ -19,12 +21,16 @@ export const ShareResolver = () => {
         if (data.url) {
           await router.push(data.url);
         } else if (data.type == "folder") {
-          setResolved(true);
+          setResolved("Folder");
         }
       })();
     },
+    onError: () => {
+      setResolved("404");
+    },
   });
 
-  if (resolved) return <Folder404 />;
+  if (resolved == "404") return <Generic404 />;
+  if (resolved == "Folder") return <Folder404 />;
   return <Loading />;
 };
