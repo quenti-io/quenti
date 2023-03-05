93a83d6ce5d7a1ac91a290e2e4f323c29128b07d
Ethan Lerner <ethan@lerners.io>
2023-03-04 21:53:58 -0500

    use proper app version

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,13 @@
 const shouldAnalyzeBundles = process.env.ANALYZE === "true";
 
-import { withAxiom } from "next-axiom";
 import { withHighlightConfig } from "@highlight-run/next";
+import { withAxiom } from "next-axiom";
 const withBundleAnalyzer = shouldAnalyzeBundles
   ? (await import("@next/bundle-analyzer")).default
   : () => undefined;
+
+import pjson from "./package.json";
+const appVersion = pjson.version;
 
 // @ts-check
 /**
@@ -15,51 +18,56 @@ const withBundleAnalyzer = shouldAnalyzeBundles
 
 /** @type {import("next").NextConfig} */
 let config = withAxiom(
-  withHighlightConfig({
-    reactStrictMode: true,
-    swcMinify: true,
-    i18n: {
-      locales: ["en"],
-      defaultLocale: "en",
+  withHighlightConfig(
+    {
+      reactStrictMode: true,
+      swcMinify: true,
+      i18n: {
+        locales: ["en"],
+        defaultLocale: "en",
+      },
+      images: {
+        domains: ["lh3.googleusercontent.com"],
+      },
+      rewrites: async () => [
+        {
+          source: "/:id(_[a-zA-Z0-9]{10})",
+          destination: "/share-resolver/:id",
+        },
+        {
+          source: "/:profile(@[a-zA-Z0-9-_]+)",
+          destination: "/profile/:profile",
+        },
+        {
+          source: "/:profile(@[a-zA-Z0-9-_]+)/folders/:slug",
+          destination: "/profile/:profile/folders/:slug",
+        },
+        {
+          source: "/:profile(@[a-zA-Z0-9-_]+)/folders/:slug/flashcards",
+          destination: "/profile/:profile/folders/:slug/flashcards",
+        },
+        {
+          source: "/:id(c[a-z0-9]{24})",
+          destination: "/sets/:id",
+        },
+        {
+          source: "/:id(c[a-z0-9]{24})/edit",
+          destination: "/sets/:id/edit",
+        },
+        {
+          source: "/:id(c[a-z0-9]{24})/flashcards",
+          destination: "/sets/:id/flashcards",
+        },
+        {
+          source: "/:id(c[a-z0-9]{24})/learn",
+          destination: "/sets/:id/learn",
+        },
+      ],
     },
-    images: {
-      domains: ["lh3.googleusercontent.com"],
-    },
-    rewrites: async () => [
-      {
-        source: "/:id(_[a-zA-Z0-9]{10})",
-        destination: "/share-resolver/:id",
-      },
-      {
-        source: "/:profile(@[a-zA-Z0-9-_]+)",
-        destination: "/profile/:profile",
-      },
-      {
-        source: "/:profile(@[a-zA-Z0-9-_]+)/folders/:slug",
-        destination: "/profile/:profile/folders/:slug",
-      },
-      {
-        source: "/:profile(@[a-zA-Z0-9-_]+)/folders/:slug/flashcards",
-        destination: "/profile/:profile/folders/:slug/flashcards",
-      },
-      {
-        source: "/:id(c[a-z0-9]{24})",
-        destination: "/sets/:id",
-      },
-      {
-        source: "/:id(c[a-z0-9]{24})/edit",
-        destination: "/sets/:id/edit",
-      },
-      {
-        source: "/:id(c[a-z0-9]{24})/flashcards",
-        destination: "/sets/:id/flashcards",
-      },
-      {
-        source: "/:id(c[a-z0-9]{24})/learn",
-        destination: "/sets/:id/learn",
-      },
-    ],
-  })
+    {
+      appVersion,
+    }
+  )
 );
 
 if (shouldAnalyzeBundles) {
