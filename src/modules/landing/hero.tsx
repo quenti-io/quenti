f2ce80319cd0a2f78a69646e4688014bb500d515
Ethan Lerner <ethan@lerners.io>
2023-04-23 13:42:56 -0400

    fix case

--- src/modules/landing/hero.tsx
+++ src/modules/landing/hero.tsx
@@ -65,7 +65,7 @@ export const Hero = () => {
                       });
                   }}
                 >
-                  Learn More
+                  Learn more
                 </Button>
               </Stack>
             </VStack>
