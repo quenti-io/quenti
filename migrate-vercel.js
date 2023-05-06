5b432dcf84be1c67d61f5c48552823e4dc5477de
Ethan Lerner <ethan@lerners.io>
2023-05-06 11:33:26 -0400

    update vercel migrate

--- migrate-vercel.js
+++ migrate-vercel.js
@@ -1,16 +1,15 @@
 "use strict";
 
 const { execSync } = require("child_process");
-const { DATABASE_URL_MIGRATE } = process.env;
+const { DIRECT_DATABASE_URL } = process.env;
 
 try {
-  if (!DATABASE_URL_MIGRATE) {
-    throw new Error("Missing DATABASE_URL_MIGRATE environment variable!");
+  if (!DIRECT_DATABASE_URL) {
+    throw new Error("Missing DIRECT_DATABASE_URL environment variable!");
   }
 
-  // Run the migrate with database connection not using pooling
-  // See: https://github.com/prisma/prisma/issues/4752
-  execSync(`DATABASE_URL=${DATABASE_URL_MIGRATE} yarn prisma migrate deploy`);
+  // RESOLVED: https://github.com/prisma/prisma/issues/4752
+  execSync("yarn prisma migrate deploy");
 } catch (error) {
   console.error(error);
   process.exit(1);
