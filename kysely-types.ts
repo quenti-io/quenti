697c51e8f5ffc84f913b09e2cf861cb8285fd58a
Ethan Lerner <ethan@lerners.io>
2023-07-01 15:00:41 -0400

    remove unused enum

--- kysely-types.ts
+++ kysely-types.ts
@@ -3,10 +3,6 @@ export type Generated<T> = T extends ColumnType<infer S, infer I, infer U>
   ? ColumnType<S, I | undefined, U>
   : ColumnType<T, T | undefined, T>;
 export type Timestamp = ColumnType<Date, Date | string, Date | string>;
-export type EnabledFeature = "ExtendedFeedbackBank";
-export const EnabledFeature = {
-  ExtendedFeedbackBank: "ExtendedFeedbackBank",
-};
 export type StudySetVisibility = "Private" | "Unlisted" | "Public";
 export const StudySetVisibility = {
   Private: "Private",
