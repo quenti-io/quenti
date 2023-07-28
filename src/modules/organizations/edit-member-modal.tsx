c412dd7c9084f93fa757febdf45c855d9697bc64
Ethan Lerner <ethan@lerners.io>
2023-07-28 13:27:37 -0400

    invite org member form

--- src/modules/organizations/edit-member-modal.tsx
+++ src/modules/organizations/edit-member-modal.tsx
@@ -1,16 +1,10 @@
-import {
-  Button,
-  ButtonGroup,
-  FormControl,
-  FormLabel,
-  useColorModeValue,
-} from "@chakra-ui/react";
+import { Button, ButtonGroup, FormControl, FormLabel } from "@chakra-ui/react";
 import type { MembershipRole } from "@prisma/client";
-import { Select } from "chakra-react-select";
 import React from "react";
 import { Modal } from "../../components/modal";
-import { api } from "../../utils/api";
 import { useOrganization } from "../../hooks/use-organization";
+import { api } from "../../utils/api";
+import { MemberRoleSelect } from "./member-role-select";
 
 export interface EditMemberModalProps {
   isOpen: boolean;
@@ -18,25 +12,6 @@ export interface EditMemberModalProps {
   id: string;
   role: MembershipRole;
 }
-
-const options: {
-  label: string;
-  value: MembershipRole;
-  isDisabled?: boolean;
-}[] = [
-  {
-    label: "Owner",
-    value: "Owner",
-  },
-  {
-    label: "Admin",
-    value: "Admin",
-  },
-  {
-    label: "Member",
-    value: "Member",
-  },
-];
 
 export const EditMemberModal: React.FC<EditMemberModalProps> = ({
   isOpen,
@@ -54,26 +29,12 @@ export const EditMemberModal: React.FC<EditMemberModalProps> = ({
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [isOpen]);
 
-  const baseBg = useColorModeValue("gray.100", "gray.750");
-  const dropdownBg = useColorModeValue("gray.200", "gray.700");
-  const chevronColor = useColorModeValue("blue.400", "blue.200");
-
   const editMemberRole = api.organizations.editMemberRole.useMutation({
     onSuccess: async () => {
       onClose();
       await utils.organizations.get.invalidate();
     },
   });
-
-  const hydrateOptions = () => {
-    if (myRole === "Admin") {
-      return options.map((x) =>
-        x.value === "Owner" ? { ...x, isDisabled: true } : x
-      );
-    }
-
-    return options;
-  };
 
   return (
     <Modal isOpen={isOpen} onClose={onClose}>
@@ -83,42 +44,7 @@ export const EditMemberModal: React.FC<EditMemberModalProps> = ({
           <Modal.Heading>Edit member</Modal.Heading>
           <FormControl>
             <FormLabel>Role</FormLabel>
-            <Select
-              selectedOptionStyle="check"
-              value={options.find((o) => o.value === role)}
-              isSearchable={false}
-              onChange={(e) => {
-                setRole(e!.value);
-              }}
-              chakraStyles={{
-                container: (provided) => ({
-                  ...provided,
-                  background: baseBg,
-                  rounded: "lg",
-                }),
-                inputContainer: () => ({
-                  width: 100,
-                  rounded: "lg",
-                }),
-                option: (props, option) => ({
-                  ...props,
-                  cursor: option.isDisabled ? "not-allowed" : "pointer",
-                  backgroundColor: option.isDisabled
-                    ? undefined
-                    : props.backgroundColor,
-                  // color: option.isDisabled ? "gray.500" : props.color,
-                }),
-                dropdownIndicator: (provided) => ({
-                  ...provided,
-                  paddingX: 2,
-                  backgroundColor: dropdownBg,
-                  color: chevronColor,
-                }),
-              }}
-              options={hydrateOptions().sort(
-                (a, b) => (a.isDisabled ? 1 : 0) - (b.isDisabled ? 1 : 0)
-              )}
-            />
+            <MemberRoleSelect value={role} onChange={setRole} myRole={myRole} />
           </FormControl>
         </Modal.Body>
         <Modal.Divider />
