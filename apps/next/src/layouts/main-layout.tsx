5eec9fc91c4f7f46ace339921dc0ac45bc13ddb6
Ethan Lerner <ethan@lerners.io>
2023-08-17 15:39:45 -0400

    add top loading bar (sad)

--- apps/next/src/layouts/main-layout.tsx
+++ apps/next/src/layouts/main-layout.tsx
@@ -1,4 +1,7 @@
 import dynamic from "next/dynamic";
+import { useRouter } from "next/router";
+import React from "react";
+import LoadingBar, { type LoadingBarRef } from "react-top-loading-bar";
 
 import { Navbar } from "../components/navbar";
 
@@ -15,8 +18,28 @@ const SignupModal = dynamic(() => import("../components/signup-modal"), {
 });
 
 export const MainLayout: React.FC<React.PropsWithChildren> = ({ children }) => {
+  const barRef = React.useRef<LoadingBarRef>(null);
+  const router = useRouter();
+
+  React.useEffect(() => {
+    router.events.on("routeChangeStart", () => {
+      barRef.current?.continuousStart(20, 750);
+    });
+    router.events.on("routeChangeComplete", () => {
+      barRef.current?.complete();
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <>
+      <LoadingBar
+        ref={barRef}
+        color="#ffa54c"
+        height={3}
+        waitingTime={500}
+        transitionTime={500}
+      />
       <Navbar />
       <GlobalShortcutLayer />
       <SignupModal />
