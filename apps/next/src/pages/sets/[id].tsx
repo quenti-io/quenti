1dc19c53a96efd36ddf197a670fc4377bee0a21e
Ethan Lerner <ethan@lerners.io>
2023-08-18 17:46:34 -0400

    super secret sauce

--- apps/next/src/pages/sets/[id].tsx
+++ apps/next/src/pages/sets/[id].tsx
@@ -1,5 +1,6 @@
 import dynamic from "next/dynamic";
 
+import { getServerAuthSession } from "@quenti/auth";
 import { HeadSeo } from "@quenti/components";
 import { prisma } from "@quenti/prisma";
 import type { GetServerSidePropsContext } from "@quenti/types";
@@ -10,13 +11,15 @@ import type { inferSSRProps } from "../../lib/infer-ssr-props";
 import { HydrateSetData } from "../../modules/hydrate-set-data";
 import { Set404 } from "../../modules/main/set-404";
 import SetLoading from "../../modules/main/set-loading";
+import { SetPrivate } from "../../modules/main/set-private";
 
 const InternalSet = dynamic(() => import("../../components/internal-set"), {
   ssr: false,
   loading: SetLoading,
 });
 
-const Set = ({ set }: inferSSRProps<typeof getServerSideProps>) => {
+const Set = ({ set, isPrivate }: inferSSRProps<typeof getServerSideProps>) => {
+  if (isPrivate) return <SetPrivate />;
   if (!set) return <Set404 />;
 
   return (
@@ -43,6 +46,9 @@ const Set = ({ set }: inferSSRProps<typeof getServerSideProps>) => {
 };
 
 export const getServerSideProps = async (ctx: GetServerSidePropsContext) => {
+  const session = await getServerAuthSession(ctx);
+  const userId = session?.user?.id;
+
   const set = await prisma.studySet.findUnique({
     where: {
       id: ctx.query?.id as string,
@@ -51,8 +57,10 @@ export const getServerSideProps = async (ctx: GetServerSidePropsContext) => {
       id: true,
       title: true,
       description: true,
+      visibility: true,
       user: {
         select: {
+          id: true,
           username: true,
           image: true,
         },
@@ -64,6 +72,10 @@ export const getServerSideProps = async (ctx: GetServerSidePropsContext) => {
       },
     },
   });
+
+  if (!set) return { props: { set: null } };
+  if (set.visibility == "Private" && set.user.id != userId)
+    return { props: { set: null, isPrivate: true } };
 
   return {
     props: {
