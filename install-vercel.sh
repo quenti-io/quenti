0c4e71220126720c1886728440b89d0d8d8eb36a
Ethan Lerner <ethan@lerners.io>
2023-08-22 11:06:13 -0400

    back to vercel install script

---
+++ install-vercel.sh
@@ -0,0 +1,39 @@
+
+if [ "$GITHUB_ACCESS_TOKEN" == "" ]; then
+  echo "GITHUB_ACCESS_TOKEN is not set"
+  exit 1
+fi
+
+set -e
+
+install () {
+  SUBMODULE_GITHUB=$1
+  SUBMODULE_PATH=$2
+
+  # Get submodule commit
+  output=`git submodule status --recursive $SU`
+  no_prefix=${output#*-}
+  COMMIT=${no_prefix% *}
+
+  # Create tmp directory
+  rm -rf tmp || true
+  mkdir tmp
+  cd tmp
+
+  # Initialize the git repo and fetch the required commit
+  git init
+  git remote add origin https://$GITHUB_ACCESS_TOKEN@$SUBMODULE_GITHUB
+  git fetch --depth=1 origin $COMMIT
+  git checkout $COMMIT
+
+  # Move tmp to the submodule path
+  cd ..
+  rm -rf tmp/.git
+  mv tmp/* $SUBMODULE_PATH/
+
+  # Clean up
+  rm -rf tmp
+}
+
+install "github.com/quenti-io/website" "packages/website"
+install "github.com/quenti-io/integrations" "packages/integrations"
