0f859f891d96330aca187df4d8fc38a3c18b1990
Ethan Lerner <ethan@lerners.io>
2023-08-29 14:17:40 -0400

    batch embedding api requests

--- packages/cortex/distractors/embeddings.ts
+++ packages/cortex/distractors/embeddings.ts
@@ -1,12 +1,26 @@
 import cohere from "cohere-ai";
 
 import { env } from "@quenti/env/server";
+import { chunkArray } from "@quenti/lib/array";
 
 if (env.COHERE_API_KEY) cohere.init(env.COHERE_API_KEY);
+
+const EMBEDDING_API_BATCH_SIZE = 96;
 
 export const generateEmbeddings = async (
   terms: string[],
 ): Promise<number[][]> => {
+  const batches = chunkArray(terms, EMBEDDING_API_BATCH_SIZE);
+  return (
+    await Promise.all(batches.map((batch) => generateEmbeddingsBatch(batch)))
+  ).flat();
+};
+
+const generateEmbeddingsBatch = async (
+  terms: string[],
+): Promise<number[][]> => {
+  console.log(`Embedding batch of ${terms.length} terms`);
+
   if (!env.COHERE_API_KEY) return [];
 
   const response = await cohere.embed({
@@ -18,6 +32,8 @@ export const generateEmbeddings = async (
       "Cohere returned a status code of " + response.statusCode.toString(),
     );
   }
+
+  console.log(`Done (${terms.length})`);
 
   return response.body.embeddings;
 };
