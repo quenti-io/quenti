79200d78776b76195c2fabd8ddf165f9e44aa051
Ethan Lerner <ethan@lerners.io>
2023-09-15 09:41:39 -0400

    proper cdn response handling

--- apps/cdn/src/index.ts
+++ apps/cdn/src/index.ts
@@ -6,6 +6,7 @@ export interface Env {
   QUENTI_ENCRYPTION_KEY: string;
   CORS_ALLOWED_ORIGINS: string;
   CLOUDINARY_CLOUD?: string;
+  CDN_ROOT_DOMAIN: string;
   // Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
   USERS_BUCKET: R2Bucket;
   ASSETS_BUCKET: R2Bucket;
@@ -34,6 +35,9 @@ export default {
       let assetUrl: string, width: number, height: number;
       try {
         const parsedUrl = new URL(pathname.replace("/image/", ""));
+        if (!parsedUrl.hostname.endsWith(env.CDN_ROOT_DOMAIN))
+          return new Response("Invalid URL", { status: 400 });
+
         parsedUrl.search = "";
         parsedUrl.hash = "";
         assetUrl = parsedUrl.toString();
@@ -51,11 +55,14 @@ export default {
           )
         : new Request(assetUrl, request);
 
-      const result = await fetch(imageRequest);
-      result.headers.set(
+      let response = await fetch(imageRequest);
+      response = new Response(response.body, response);
+      response.headers.set(
         "Cache-Control",
         "s-maxage=300 stale-while-revalidate=59",
       );
+
+      return response;
     }
 
     if (request.method !== "PUT")
