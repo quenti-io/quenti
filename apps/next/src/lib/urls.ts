e011ea458251b6e7b83123ac1e2d4c76c3dcdabe
Ethan Lerner <ethan@lerners.io>
2023-10-16 15:09:30 -0400

    fix double slash callback url

--- apps/next/src/lib/urls.ts
+++ apps/next/src/lib/urls.ts
@@ -8,12 +8,15 @@ export const getSafeRedirectUrl = (url: string) => {
   let safeUrl = url;
 
   if (!/^https?:\/\//.test(url)) {
-    safeUrl = `${env.NEXT_PUBLIC_BASE_URL}/${url}`;
+    safeUrl = `${env.NEXT_PUBLIC_BASE_URL}/${url}`.replace(
+      /([^:])(\/\/+)/g,
+      "$1/",
+    );
   }
   const parsed = new URL(safeUrl);
 
   if (new URL(env.NEXT_PUBLIC_BASE_URL).origin !== parsed.origin) {
-    return env.NEXT_PUBLIC_BASE_URL;
+    return `${env.NEXT_PUBLIC_BASE_URL}/home`;
   }
 
   return safeUrl;
