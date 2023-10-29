a09ac0e23d35a1f42b43b03cb5ec1f13af489a5b
Ethan Lerner <ethan@lerners.io>
2023-10-29 12:21:48 -0400

    start of rich text editing

---
+++ apps/next/src/modules/editor/card/rich-text-property.tsx
@@ -0,0 +1,29 @@
+import { IconButton, useColorModeValue } from "@chakra-ui/react";
+
+import type { TablerIconsProps } from "@tabler/icons-react";
+
+export interface RichTextPropertyProps {
+  icon: React.FC<TablerIconsProps>;
+  label: string;
+  onClick: () => void;
+  isActive: boolean | undefined;
+}
+
+export const RichTextProperty: React.FC<RichTextPropertyProps> = ({
+  icon: Icon,
+  onClick,
+  isActive = false,
+}) => {
+  const activeBg = useColorModeValue("gray.200", "gray.700");
+
+  return (
+    <IconButton
+      icon={<Icon size={16} />}
+      aria-label="Bold"
+      rounded="full"
+      onMouseDown={(e) => e.preventDefault()}
+      onClick={() => onClick()}
+      bg={isActive ? activeBg : undefined}
+    />
+  );
+};
--- apps/next/src/modules/editor/editor-config.tsx
+++ apps/next/src/modules/editor/editor-config.tsx
@@ -1,7 +1,13 @@
+import Bold from "@tiptap/extension-bold";
 import Document from "@tiptap/extension-document";
+import Highlight from "@tiptap/extension-highlight";
+import History from "@tiptap/extension-history";
+import Italic from "@tiptap/extension-italic";
 import Paragraph from "@tiptap/extension-paragraph";
+import Strike from "@tiptap/extension-strike";
 import Text from "@tiptap/extension-text";
 import Typography from "@tiptap/extension-typography";
+import Underline from "@tiptap/extension-underline";
 import { type useEditor } from "@tiptap/react";
 
 import { EmojiReplacer } from "./extensions/emoji-replacer";
@@ -16,11 +22,25 @@ const boxShadow =
 export const editorConfig = (
   tabIndex?: number,
 ): Parameters<typeof useEditor>[0] => ({
-  extensions: [Document, Paragraph, Text, Typography, EmojiReplacer],
+  extensions: [
+    Document,
+    Paragraph,
+    Text,
+    Typography,
+    Bold,
+    Italic,
+    Strike,
+    Underline,
+    Highlight,
+    EmojiReplacer,
+    History.configure({
+      depth: 20,
+    }),
+  ],
   editorProps: {
     attributes: {
       class: `focus:outline-none py-[7px] border-b-[1px] transition-[border,box-shadow] ${grayBorder} ${darkGrayBorder} ${blueBorder} ${darkBlueBorder} ${boxShadow}`,
-      ...(tabIndex !== undefined ? { tabIndex: `${tabIndex}` } : {}),
+      ...(tabIndex !== undefined ? { tabindex: `${tabIndex}` } : {}),
     },
   },
 });
