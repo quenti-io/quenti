d404e529b1cea2b04fdcb56466da801ad719df5a
Ethan Lerner <ethan@lerners.io>
2023-11-23 13:16:13 -0500

    delete cdn assets

--- packages/trpc/server/routers/study-sets/delete.handler.ts
+++ packages/trpc/server/routers/study-sets/delete.handler.ts
@@ -1,3 +1,8 @@
+import { env } from "@quenti/env/server";
+import { deleteStudySetAssets } from "@quenti/images/server";
+
+import { TRPCError } from "@trpc/server";
+
 import type { NonNullableUserContext } from "../../lib/types";
 import type { TDeleteSchema } from "./delete.schema";
 
@@ -7,12 +12,42 @@ type DeleteOptions = {
 };
 
 export const deleteHandler = async ({ ctx, input }: DeleteOptions) => {
-  return await ctx.prisma.studySet.delete({
+  const studySet = await ctx.prisma.studySet.findUnique({
     where: {
       id_userId: {
         id: input.studySetId,
         userId: ctx.session.user.id,
       },
+    },
+    select: {
+      id: true,
+    },
+  });
+
+  if (!studySet) {
+    throw new TRPCError({
+      code: "NOT_FOUND",
+    });
+  }
+
+  const termWithCdnAsset = await ctx.prisma.term.findFirst({
+    where: {
+      studySetId: input.studySetId,
+      AND: {
+        assetUrl: {
+          startsWith: env.ASSETS_BUCKET_URL,
+        },
+      },
+    },
+  });
+
+  if (termWithCdnAsset) {
+    await deleteStudySetAssets(studySet.id);
+  }
+
+  return await ctx.prisma.studySet.delete({
+    where: {
+      id: input.studySetId,
     },
   });
 };
--- packages/trpc/server/routers/terms/remove-image.handler.ts
+++ packages/trpc/server/routers/terms/remove-image.handler.ts
@@ -1,3 +1,6 @@
+import { env } from "@quenti/env/server";
+import { deleteTermAsset } from "@quenti/images/server";
+
 import { TRPCError } from "@trpc/server";
 
 import type { NonNullableUserContext } from "../../lib/types";
@@ -27,6 +30,24 @@ export const removeImageHandler = async ({
       code: "NOT_FOUND",
     });
   }
+
+  const term = await ctx.prisma.term.findUnique({
+    where: {
+      id_studySetId: {
+        id: input.id,
+        studySetId: input.studySetId,
+      },
+    },
+    select: {
+      id: true,
+      assetUrl: true,
+    },
+  });
+
+  if (!term) throw new TRPCError({ code: "NOT_FOUND" });
+
+  if (env.ASSETS_BUCKET_URL && term.assetUrl?.startsWith(env.ASSETS_BUCKET_URL))
+    await deleteTermAsset(studySet.id, term.id);
 
   return await ctx.prisma.term.update({
     where: {
