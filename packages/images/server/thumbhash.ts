6294ba691460cf16bb7963eacec4bbf7c1fb16d2
Ethan Lerner <ethan@lerners.io>
2023-11-25 19:05:23 -0500

    fix: use cgi endpoint for thumbhash generation

--- packages/images/server/thumbhash.ts
+++ packages/images/server/thumbhash.ts
@@ -3,14 +3,19 @@ import { rgbaToThumbHash } from "thumbhash";
 
 import { env } from "@quenti/env/client";
 
+const transform = (url: string, transform: string) => {
+  const v = url.includes("?") ? "&" : "?";
+  return `${env.NEXT_PUBLIC_CGI_ENDPOINT}/${url}${v}twic=v1/${transform}`;
+};
+
 export const thumbhashFromCdn = async (
   url: string,
   width = 256,
   height = 256,
 ) => {
-  if (!env.NEXT_PUBLIC_CDN_WORKER_ENDPOINT) return null;
+  if (!env.NEXT_PUBLIC_CGI_ENDPOINT) return null;
 
-  const endpoint = `${env.NEXT_PUBLIC_CDN_WORKER_ENDPOINT}/image/${url}&w=${width}&h=${height}`;
+  const endpoint = transform(url, `cover=${width}x${height}`);
 
   const response = await fetch(endpoint);
   const buffer = await response.arrayBuffer();
