bc5acdaafe446f6a6038a8c1802489b1c02ba1aa
Ethan Lerner <ethan@lerners.io>
2023-12-24 10:21:18 -0500

    feat: empty assignments page

--- apps/next/src/modules/classes/class-empty.tsx
+++ apps/next/src/modules/classes/class-empty.tsx
@@ -1,14 +1,26 @@
-import { VStack } from "@chakra-ui/react";
+import { Box, VStack } from "@chakra-ui/react";
 
 import { GhostMessage } from "../../components/ghost-message";
 
 export const ClassEmpty = () => {
   return (
-    <VStack mt="10" px="-4">
-      <GhostMessage
-        message="There's nothing here yet"
-        subheading="Check back here once your teacher adds some content to the class."
-      />
-    </VStack>
+    <Box
+      w="full"
+      rounded="xl"
+      borderWidth="2px"
+      borderColor="gray.200"
+      _dark={{
+        borderColor: "gray.750",
+      }}
+      pb="10"
+      px="4"
+    >
+      <VStack mt="10" px="-4">
+        <GhostMessage
+          message="There's nothing here yet"
+          subheading="Check back here once your teacher adds some content to the class."
+        />
+      </VStack>
+    </Box>
   );
 };
---
+++ apps/next/src/modules/classes/pages/class-assignments.tsx
@@ -0,0 +1,11 @@
+import { Stack } from "@chakra-ui/react";
+
+import { ClassEmpty } from "../class-empty";
+
+export const ClassAssignments = () => {
+  return (
+    <Stack>
+      <ClassEmpty />
+    </Stack>
+  );
+};
