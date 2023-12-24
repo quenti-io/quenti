5ca3fc645b25ab93e189fafcf13f24dc38d2edd6
Ethan Lerner <ethan@lerners.io>
2023-12-24 12:29:46 -0500

    feat: start of assignments feed

---
+++ apps/next/src/modules/classes/assignments/assignment-card.tsx
@@ -0,0 +1,109 @@
+import type { AssignmentType } from "@quenti/prisma/client";
+
+import { Box, HStack, Heading, Stack, Text } from "@chakra-ui/react";
+
+import { IconUsersGroup } from "@tabler/icons-react";
+
+export interface AssignmentCardProps {
+  for: "Teacher" | "Student";
+  id: string;
+  type: AssignmentType;
+  name: string;
+  createdAt: Date;
+  availableAt: Date;
+  dueAt?: Date;
+  published?: boolean;
+  submissions?: number;
+  section?: {
+    id: string;
+    name: string;
+    students: number;
+  };
+  submission?: {
+    id: string;
+    startedAt: Date;
+    submittedAt?: Date;
+  };
+}
+
+export const AssignmentCard = () => {
+  return (
+    <Box
+      px="6"
+      py="4"
+      bg="white"
+      borderColor="gray.200"
+      _hover={{
+        transform: "translateY(-2px)",
+        borderBottomColor: "blue.300",
+      }}
+      _dark={{
+        borderColor: "gray.700",
+        bg: "gray.800",
+        _hover: {
+          borderBottomColor: "blue.300",
+        },
+      }}
+      rounded="lg"
+      borderWidth="2px"
+      shadow="md"
+      sx={{
+        "&:has(:focus-visible)": {
+          borderColor: "blue.300",
+          transform: "translateY(-2px)",
+          _dark: {
+            borderColor: "blue.300",
+          },
+        },
+      }}
+      transition="all ease-in-out 150ms"
+    >
+      <HStack spacing="4">
+        <Box
+          p="10px"
+          color="blue.300"
+          position="relative"
+          overflow="hidden"
+          rounded="full"
+          outline="2px solid"
+          outlineOffset={-2}
+          outlineColor="gray.100"
+          _dark={{
+            outlineColor: "gray.750",
+          }}
+          shadow="md"
+        >
+          <Box
+            p="10px"
+            position="absolute"
+            top="0"
+            left="0"
+            w="full"
+            h="full"
+            rounded="full"
+            bg="rgba(75, 131, 255, 0.1)"
+            filter="blur(3px)"
+            color="blue.200"
+          >
+            <Box opacity={0.75}>
+              <IconUsersGroup size={24} />
+            </Box>
+          </Box>
+          <IconUsersGroup size={24} />
+        </Box>
+        <Stack spacing="1">
+          <Heading fontSize="lg">Final Midterm Study Guide</Heading>
+          <Text
+            fontSize="sm"
+            color="gray.600"
+            _dark={{
+              color: "gray.400",
+            }}
+          >
+            Today at 2:00 PM
+          </Text>
+        </Stack>
+      </HStack>
+    </Box>
+  );
+};
--- apps/next/src/modules/classes/pages/class-assignments.tsx
+++ apps/next/src/modules/classes/pages/class-assignments.tsx
@@ -1,11 +1,26 @@
-import { Stack } from "@chakra-ui/react";
+import { Box, HStack, Heading, Stack } from "@chakra-ui/react";
 
-import { ClassEmpty } from "../class-empty";
+import { LoadingSearch } from "../../../components/loading-search";
+import { AssignmentCard } from "../assignments/assignment-card";
 
 export const ClassAssignments = () => {
   return (
-    <Stack>
-      <ClassEmpty />
+    <Stack spacing="8" mt="8">
+      <LoadingSearch
+        value=""
+        placeholder="Search assignments"
+        onChange={() => {}}
+      />
+      <Stack spacing="6">
+        <HStack spacing="4">
+          <Heading fontSize="2xl">Today</Heading>
+          <Box flex="1" h="2px" bg="gray.100" _dark={{ bg: "gray.750" }} />
+        </HStack>
+        <Stack spacing="4">
+          <AssignmentCard />
+          <AssignmentCard />
+        </Stack>
+      </Stack>
     </Stack>
   );
 };
