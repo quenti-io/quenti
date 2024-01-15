99b875693b05f510314bab74ebccfda9e8896bdb
Ethan Lerner <ethan@lerners.io>
2024-01-15 09:22:23 -0500

    fix: settings wrapper breaking safari

--- apps/next/src/modules/organizations/settings-wrapper.tsx
+++ apps/next/src/modules/organizations/settings-wrapper.tsx
@@ -1,6 +1,7 @@
 import {
   Flex,
   Grid,
+  GridItem,
   Heading,
   SkeletonText,
   Stack,
@@ -26,25 +27,27 @@ export const SettingsWrapper: React.FC<
       gap={{ base: 4, md: 16 }}
       maxW="4xl"
     >
-      <Stack spacing="1" h="full">
-        <Flex h="6" alignItems="center">
-          <SkeletonText noOfLines={1} skeletonHeight="5" isLoaded={isLoaded}>
-            <Heading size="md">{heading}</Heading>
-          </SkeletonText>
-        </Flex>
-        <Flex alignItems="center" pb="2">
-          <SkeletonText
-            noOfLines={noOfLines}
-            skeletonHeight={3}
-            isLoaded={isLoaded}
-          >
-            <Text fontSize="sm" color={mutedColor}>
-              {description}
-            </Text>
-          </SkeletonText>
-        </Flex>
-      </Stack>
-      {children}
+      <GridItem>
+        <Stack spacing="1" h="full">
+          <Flex h="6" alignItems="center">
+            <SkeletonText noOfLines={1} skeletonHeight="5" isLoaded={isLoaded}>
+              <Heading size="md">{heading}</Heading>
+            </SkeletonText>
+          </Flex>
+          <Flex alignItems="center" pb="2">
+            <SkeletonText
+              noOfLines={noOfLines}
+              skeletonHeight={3}
+              isLoaded={isLoaded}
+            >
+              <Text fontSize="sm" color={mutedColor}>
+                {description}
+              </Text>
+            </SkeletonText>
+          </Flex>
+        </Stack>
+      </GridItem>
+      <GridItem>{children}</GridItem>
     </Grid>
   );
 };
