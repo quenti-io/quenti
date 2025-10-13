1ebd803190a0c13947d7bd100b859da35367db12
William Paek <wpaek@hawaii.edu>
2025-10-12 18:58:01 -1000

    [Feature Request] LaTex Support

--- packages/components/script-formatter.tsx
+++ packages/components/script-formatter.tsx
@@ -1,10 +1,22 @@
 import React, { useMemo } from "react";
 
 import { formatScripts } from "@quenti/lib/scripts";
+import { LatexFormatter } from "./latex-formatter";
 
 export const ScriptFormatter = ({ children }: { children: string }) => {
   const formatted = useMemo(() => formatScripts(children), [children]);
 
+  // Check if the text contains LaTeX expressions
+  const hasLatex = useMemo(() => {
+    return /(\$\$[\s\S]*?\$\$|\$[^$\n]*?\$)/.test(children);
+  }, [children]);
+
+  // If LaTeX is detected, use LatexFormatter which will handle both LaTeX and regular text
+  if (hasLatex) {
+    return <LatexFormatter>{children}</LatexFormatter>;
+  }
+
+  // Otherwise, use the original script formatting
   return (
     <>
       {formatted.map((item, index) => (
